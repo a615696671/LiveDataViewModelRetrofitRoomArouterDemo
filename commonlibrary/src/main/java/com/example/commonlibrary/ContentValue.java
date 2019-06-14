c34a5b72f29d0b3dcb5201f2d9ba69ba30a1bb39
a615696671 <615696671@qq.com>
2019-06-14 11:24:27 +0800

    修改bugly  key的加入方式，用gradle方式动态替换

--- commonlibrary/src/main/java/com/example/commonlibrary/ContentValue.java
+++ commonlibrary/src/main/java/com/example/commonlibrary/ContentValue.java
@@ -2,6 +2,4 @@ package com.example.commonlibrary;
 
 public class ContentValue {
     public  static final String BASE_URL="http://192.168.16.148:8088/";
-
-    public  static final String BUGLYID="6d22aa302e";
 }
