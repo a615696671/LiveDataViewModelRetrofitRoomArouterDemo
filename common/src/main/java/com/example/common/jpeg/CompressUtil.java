831c2102b6d6cad23307c6161a211019beac55ef
a615696671 <615696671@qq.com>
2019-07-02 17:30:14 +0800

    jepg  so打入不进去apk中？？

--- common/src/main/java/com/example/common/jpeg/CompressUtil.java
+++ common/src/main/java/com/example/common/jpeg/CompressUtil.java
@@ -5,6 +5,8 @@ import android.graphics.Bitmap;
 public class CompressUtil {
     static {
         System.loadLibrary("jpeg-lib");
+        System.loadLibrary("jpeg-1");
+        System.loadLibrary("turbojpeg-1");
     }
     public native static int compressBitmap(Bitmap bitmap, int quality, String destFile);
 }
