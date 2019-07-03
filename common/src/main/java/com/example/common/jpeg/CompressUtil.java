99f99f59c1204c4963ed478a8258c0930eeae8b3
a615696671 <615696671@qq.com>
2019-07-03 19:00:59 +0800

    **

--- common/src/main/java/com/example/common/jpeg/CompressUtil.java
+++ common/src/main/java/com/example/common/jpeg/CompressUtil.java
@@ -1,12 +1,13 @@
 package com.example.common.jpeg;
 
 import android.graphics.Bitmap;
-
 public class CompressUtil {
     static {
         System.loadLibrary("jpeg-lib");
-        System.loadLibrary("jpeg-1");
-        System.loadLibrary("turbojpeg-1");
     }
+    //无论怎么压缩，file的大小小下去了，但是转换成bitmap大小又回去了。
+    // 上传时候做图片压缩可以节省服务器空间大小。在jpeg图片压缩中好多代码都没有返回bitmap的原因也在于此。
     public native static int compressBitmap(Bitmap bitmap, int quality, String destFile);
+
+
 }
--- testmodule/src/main/java/com/example/testmodule/jpeg/JpegTestActivity.java
+++ testmodule/src/main/java/com/example/testmodule/jpeg/JpegTestActivity.java
@@ -7,21 +7,17 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Environment;
-import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.TextView;
+
 
 import com.alibaba.android.arouter.facade.annotation.Route;
-import com.example.base.jpeg.CompressUtil;
-import com.example.common.ArouterConstant;
-import com.example.testmodule.R;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import com.example.common.ArouterConstant;
+import com.example.common.jpeg.CompressUtil;
+import com.example.testmodule.R;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 
 @Route(path = ArouterConstant.JpegTestActivity)
 public class JpegTestActivity extends AppCompatActivity {
@@ -39,47 +35,16 @@ public class JpegTestActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         bitmap = BitmapFactory.decodeStream(open);
-    }
-    private void compressByDefault(Bitmap bitmap, int quality) {
-        File file = new File(getSaveLocation() + "/compress2.png");
-        if (file.exists()) {
-            try {
-                file.delete();
-                file.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        try {
-            OutputStream stream = new FileOutputStream(file);
-            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, stream);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
+        TextView sizeView = findViewById(R.id.tv);
+        sizeView.setText(bitmap.getByteCount()+"");
     }
 
     private String getSaveLocation() {
         return Environment.getExternalStorageDirectory().getAbsolutePath();
     }
+public void compressNative(View view){
+     String result = getSaveLocation() + "/compress.png";
+     CompressUtil.compressBitmap(this.bitmap, qu, result);
+}
 
-    public void compressNative(View view) {
-        String result = getSaveLocation() + "/compress.png";
-        Log.e("C_TAG", result);
-        long time = System.currentTimeMillis();
-        int i = CompressUtil.compressBitmap(bitmap, qu, result);
-        Log.e("C_TAG", "Native" + (System.currentTimeMillis() - time));
-        if (i == 1) {
-            Toast.makeText(this, "压缩完成，耗时：" + (System.currentTimeMillis() - time) + "毫秒", Toast.LENGTH_LONG).show();
-        } else {
-            Toast.makeText(this, "压缩失败", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    public void compressSystem(View view) {
-        long time = System.currentTimeMillis();
-        compressByDefault(bitmap, qu);
-        Log.e("C_TAG", "Java" + (System.currentTimeMillis() - time));
-        Toast.makeText(this, "压缩完成，耗时：" + (System.currentTimeMillis() - time) + "毫秒", Toast.LENGTH_LONG).show();
-    }
 }
