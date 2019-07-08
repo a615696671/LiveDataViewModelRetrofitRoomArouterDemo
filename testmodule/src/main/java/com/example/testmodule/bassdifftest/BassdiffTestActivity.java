e5ea21f76724ac910c9ddcaef7e30ab1925c7dd3
a615696671 <615696671@qq.com>
2019-07-08 14:07:44 +0800

    加入bassdiff,实现bassdiff源码编译。功能尚未完成待验证

--- testmodule/src/main/java/com/example/testmodule/bassdifftest/BassdiffTestActivity.java
+++ testmodule/src/main/java/com/example/testmodule/bassdifftest/BassdiffTestActivity.java
@@ -2,14 +2,29 @@ package com.example.testmodule.bassdifftest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.StrictMode;
 import android.view.View;
+import android.widget.Toast;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.example.base.ActivityLifeCallBack;
+import com.example.base.domain.executor.Executor;
+import com.example.base.domain.executor.impl.ThreadExecutor;
+import com.example.base.domain.interactors.base.AbstractInteractor;
+import com.example.base.domain.threading.MainThreadImpl;
+import com.example.base.utils.AppUtils;
 import com.example.common.ArouterConstant;
+import com.example.common.BaseApplication;
 import com.example.common.bassdiff.BassdiffUtils;
+import com.example.common.map.LocationService;
 import com.example.testmodule.R;
+import com.squareup.leakcanary.LeakCanary;
+import com.tencent.bugly.Bugly;
+
 @Route(path = ArouterConstant.BassdiffTestActivity)
 public class BassdiffTestActivity extends AppCompatActivity {
 
@@ -18,32 +33,46 @@ public class BassdiffTestActivity extends AppCompatActivity {
     //新版本
     String newp = getSaveLocation()+ "/newApk.apk";
     //差分包
-    String patch =getSaveLocation() + "patchApk.patch";
+    String patch =getSaveLocation() + "/patchApk.patch";
     //旧版apk和差分包合并生成的新版apk
-    String tmp = getSaveLocation()  + "plusNewApk.apk";
+    String tmp = getSaveLocation()  + "/plusNewApk.apk";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bassdiff_test);
         findViewById(R.id.btn2).setOnClickListener(view -> {
+
              //开始合并
-            BassdiffUtils.patch(old,newp,patch);
+            ThreadExecutor.getInstance().execute(new AbstractInteractor(interactor -> {
+                interactor.execute();
+            }, MainThreadImpl.getInstance()) {
+                @Override
+                public void run() {
+                    BassdiffUtils.patch(old,newp,patch);
+                    mMainThread.post(() -> Toast.makeText(BassdiffTestActivity.this, "合并成功!", Toast.LENGTH_SHORT).show());
+                }
+            });
         });
 
-
         findViewById(R.id.btn1).setOnClickListener(view -> {
-             //开始差分
-            BassdiffUtils.diff(old,tmp,patch);
 
+                    ThreadExecutor.getInstance().execute(new AbstractInteractor(interactor -> {
+                        interactor.execute();
 
+                    }, MainThreadImpl.getInstance()) {
+                @Override
+                public void run() {
+                    //开始差分
+                    BassdiffUtils.diff(old,tmp,patch);
+                   mMainThread.post(() -> Toast.makeText(BassdiffTestActivity.this, "差分成功!", Toast.LENGTH_SHORT).show());
+                }
+            });
         });
 
     }
     private String getSaveLocation() {
         return Environment.getExternalStorageDirectory().getAbsolutePath();
     }
-
-
 
 }
